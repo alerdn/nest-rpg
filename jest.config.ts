6cd8de4136a292059f98ea0d09daa98621228b9c
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2025-11-18 13:39:53 -0400

    init

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+const config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^test/(.*)$': '<rootDir>/test/$1',
+  },
+};
+
+export default config;
